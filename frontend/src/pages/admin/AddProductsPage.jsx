a27da320b405ef4f43e2deefcd195def2a8a72ad
AliSherTR <94902748+AliSherTR@users.noreply.github.com>
2024-03-07 15:35:06 +0500

    add product form

--- frontend/src/pages/admin/AddProductsPage.jsx
+++ frontend/src/pages/admin/AddProductsPage.jsx
@@ -40,7 +40,123 @@ export default function AddProductsPage() {
             </h1>
             {/* <form className="flex flex-col gap-1" encType="multipart/form-data"> */}
             <form encType="multipart/form-data">
-                <div className=" flex gap-3 justify-between mb-5">
+                <div className=" flex items-start gap-2">
+                    <div className="flex-1">
+                        <div className="px-2 py-3 bg-white rounded-md mb-2">
+                            <h1 className=" font-bold mb-5">Description</h1>
+                            <div>
+                                <label htmlFor="product_name">
+                                    Product Name
+                                </label>
+                                <input
+                                    type="text"
+                                    id="product_name"
+                                    className=" block mt-2 px-3 py-3 min-w-full rounded-md border  "
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="product_brand">
+                                    Brand Name
+                                </label>
+                                <input
+                                    type="text"
+                                    id="product_brand"
+                                    className=" block mt-2 px-3 py-3 min-w-full rounded-md border  "
+                                />
+                            </div>
+                        </div>
+                        <div className="flex-1  px-2 py-3 bg-white rounded-md col-start-1 col-end-">
+                            <h1 className=" font-bold mb-5">Description</h1>
+                            <label htmlFor="product_desc">
+                                Product Details
+                            </label>
+                            <textarea
+                                id="product_desc"
+                                className=" block mt-2 px-3 py-3  min-w-full rounded-md border"
+                                rows={10}
+                            />
+                        </div>
+                    </div>
+
+                    <div className=" flex-1 px-2 py-3 bg-white rounded-md row-span-full row-end-12 col-start-2 col-end-2 ">
+                        <div className=" mb-5">
+                            <h1 className=" font-bold mb-5">Category</h1>
+                            <label
+                                className="block mb-2 text-sm font-medium text-gray-900"
+                                htmlFor="multiple_files"
+                            >
+                                Product Images
+                            </label>
+                            <input
+                                type="file"
+                                className="block py-3 px-2 w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none  dark:border-gray-600 dark:placeholder-gray-400"
+                                id="multiple_files"
+                                multiple
+                            />
+                        </div>
+                        <div>
+                            <h1 className=" font-bold mb-5">Category</h1>
+                            <div className=" mb-3">
+                                <label htmlFor="product_category">
+                                    Product category
+                                </label>
+                                <select
+                                    type="text"
+                                    id="product_category"
+                                    className=" block mt-2 px-3 py-3 min-w-full rounded-md border  "
+                                >
+                                    <option value="">Mobile</option>
+                                    <option value="">Laptop</option>
+                                    <option value="">Tablets</option>
+                                    <option value="">Audio</option>
+                                    <option value="">Camera</option>
+                                    <option value="">Smart Watches</option>
+                                    <option value="">Others</option>
+                                </select>
+                            </div>
+
+                            <div className=" mb-3 flex gap-3 ">
+                                <div className=" flex-1">
+                                    <label htmlFor="product_price">
+                                        Base Price
+                                    </label>
+                                    <input
+                                        type="Number"
+                                        id="product_price"
+                                        className=" block mt-2 px-3 py-3 min-w-full rounded-md border  "
+                                    />
+                                </div>
+                                <div className=" flex-1">
+                                    <label htmlFor="product_discount">
+                                        Discount Price
+                                    </label>
+                                    <input
+                                        type="number"
+                                        id="product_discount"
+                                        className=" block mt-2 px-3 py-3 min-w-full rounded-md border  "
+                                    />
+                                </div>
+                            </div>
+                            <div className=" mb-3">
+                                <label htmlFor="product_qty">
+                                    Total Quantity
+                                </label>
+                                <input
+                                    type="Number"
+                                    id="product_qty"
+                                    className="block mt-2 px-3 py-3 min-w-full rounded-md border"
+                                />
+                            </div>
+                            <AdminAddBtn
+                                text={"Add"}
+                                onclick={handleFormSubmit}
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                {/* <div className=" grid grid-cols-2 grid-rows-12 gap-3  h-[70vh]"></div> */}
+                {/* <div className=" flex gap-3 justify-between mb-5">
                     <div className="flex-1 px-2 py-3 bg-white rounded-md self-start ">
                         <h1 className=" font-bold mb-5">Description</h1>
                         <div>
@@ -60,30 +176,29 @@ export default function AddProductsPage() {
                             />
                         </div>
                     </div>
-                    <div className="flex-1 px-2 py-3 bg-white rounded-md ">
-                        <h1 className=" font-bold mb-5">Description</h1>
-                        <label htmlFor="product_desc">Product Details</label>
-                        <textarea
-                            id="product_desc"
-                            className=" block mt-2 px-3 py-3 min-w-full rounded-md border   "
-                            rows={8}
-                        />
-                    </div>
-                </div>
-                <div className=" flex gap-3 justify-between mb-5">
-                    <div className="flex-1 px-2 py-3 bg-white rounded-md self-start ">
+                    <div className="flex-1 px-2 py-3 bg-white rounded-md  ">
                         <h1 className=" font-bold mb-5">Category</h1>
                         <div>
-                            <label htmlFor="product_name">
+                            <label htmlFor="product_category">
                                 Product category
                             </label>
                             <select
                                 type="text"
-                                id="product_name"
+                                id="product_category"
                                 className=" block mt-2 px-3 py-3 min-w-full rounded-md border  "
-                            />
+                            >
+                                <option value="">Mobile</option>
+                                <option value="">Laptop</option>
+                                <option value="">Tablets</option>
+                                <option value="">Audio</option>
+                                <option value="">Camera</option>
+                                <option value="">Smart Watches</option>
+                                <option value="">Others</option>
+                            </select>
                         </div>
                     </div>
+                </div>
+                <div className=" flex gap-3 justify-between mb-5">
                     <div className="flex-1 px-2 py-3 bg-white rounded-md ">
                         <h1 className=" font-bold mb-5">Description</h1>
                         <label htmlFor="product_desc">Product Details</label>
@@ -93,7 +208,8 @@ export default function AddProductsPage() {
                             rows={8}
                         />
                     </div>
-                </div>
+                    <div className=" flex-1"></div>
+                </div> */}
 
                 {/* <div className="flex gap-2">
                     <h1 className=" font-semibold text-lg">Description</h1>
--- frontend/src/ui/AdminAddBtn.jsx
+++ frontend/src/ui/AdminAddBtn.jsx
@@ -1,7 +1,7 @@
 export default function AdminAddBtn({ text, onclick }) {
     return (
         <button
-            className=" px-4 py-2 bg-green-400 rounded-lg"
+            className=" px-4 py-2 w-full bg-green-400 rounded-lg"
             onClick={onclick}
         >
             {" "}
